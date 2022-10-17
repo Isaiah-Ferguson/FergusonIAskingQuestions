4099c5451592a428abf729072362c69f78fbbb87
Isaiah-Ferguson <crestice@yahoo.com>
2022-10-17 15:13:12 -0700

    Data has been Validated

--- Program.cs
+++ Program.cs
@@ -7,16 +7,22 @@
 Console.Clear();
 
 string answer = "";
+bool Troof1 = true;
+bool Troof2 = true;
+int NewAnswer = 0;
 
 while(answer != "end"){
 Console.WriteLine("Goodmorning, what's your name?");
 string response = Console.ReadLine();
+Troof1 = Int32.TryParse(answer, out NewAnswer);
 
+if (Troof1 == true){ Console.WriteLine("Those are just numbers! don't lie to me!!");}
+else{
 Console.WriteLine($"{response}? Wow what a mysterious name!! What time did you wake up?");
 string response1 = Console.ReadLine();
 
 Console.WriteLine($"{response1} wow I woke up about 10 minutes after {response1}");
-
+}
 Console.WriteLine("Type End to Terminate the program or press enter to replay.");
 answer = Console.ReadLine().ToLower();
 }
